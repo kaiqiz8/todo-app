71d5f62f119ba5210c71d3d1ce84b8780bd3c2b9
kaiqiz8 <kzhang49@binghamton.edu>
2023-04-26 13:28:36 -0400

    add fifth component within first component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FirstComponent from './components/learning-examples/FirstComponent'
 import SecondComponent from './components/learning-examples/SecondComponent'
 import ThirdComponent from './components/learning-examples/ThirdComponent'
 import FourthComponent from './components/learning-examples/FourthComponent'
+import { FifthComponent } from './components/learning-examples/FirstComponent';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <SecondComponent></SecondComponent>
       <ThirdComponent/>
       <FourthComponent/>
+      <FifthComponent/>
     </div>
   );
 }
