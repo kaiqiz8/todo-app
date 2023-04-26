a9621e2b4fa107eba34019643b63c2bbd5ee69c9
kaiqiz8 <kzhang49@binghamton.edu>
2023-04-26 18:15:42 -0400

    add/subtract different values in counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,13 +1,57 @@
-export default function Counter() {
+import { useState } from 'react';
+import './Counter.css'
+import { getByLabelText } from '@testing-library/react';
+import {PropTypes} from 'prop-types';
+export default function Counter({by}) {
+
+    // const buttonStyle = {
+    //     fontSize:"16px",
+    //     backgroundColor: "#00a5ab",
+    //     width: "100px",
+    //     margin: "10px",
+    //     color: "white",
+    //     padding: "15px",
+    //     borderRadius: "30px"};
+
+    const [count, setCount] = useState(0);
+    console.log(by);
 
     function incrementCounterFunction() {
+        setCount(count + by);
+        console.log(count);
         console.log('increment clicked');
+    };
+
+    function decrementCounterFunction() {
+        setCount(count - by);
+        console.log(count);
+        console.log('decrement clicked');
     }
     return (
-        <div className="Counter">
-            <span className="count">0</span>
-            <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
+        <div className="counter">
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton" 
+                        onClick={incrementCounterFunction}
+                        // style={buttonStyle}
+                > + {by}</button>
+                <button className="counterButton" 
+                        onClick={decrementCounterFunction}
+                        // style={buttonStyle}
+                > - {by}</button>
+            </div>
+
+            
         
         </div>
     )
+     
+}
+
+Counter.propTypes = {
+    by: PropTypes.number
+}
+
+Counter.defaultProps = {
+    by: 5
 }
