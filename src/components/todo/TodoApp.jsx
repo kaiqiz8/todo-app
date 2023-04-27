d89f6b0c9512663d15c68c08f908d3cced43d728
kaiqiz8 <kzhang49@binghamton.edu>
2023-04-27 14:02:31 -0400

    add header

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,9 +5,10 @@ import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <HeaderComponent />
+           
 
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
                     <Route path='/' element={<LoginComponent />} />
                     <Route path='/login' element={<LoginComponent />}/>
@@ -142,17 +143,48 @@ function ListTodoComponent() {
 
 function HeaderComponent() {
     return (
-        <div className="header">
-            Header <hr/>
-        </div>
+        // <header className="header">
+        //     <div className="container" >
+        //         <ul className="navbar-nav">
+        //             <li className="nav-item">Todo App</li>
+        //             <li className="nav-item"><Link className='nav-link' to="/welcome/Kaiqi">Home</Link></li>
+        //             <li className="nav-item"><Link className='nav-link' to="/todos">Todos</Link></li>
+        //             <li className="nav-item"><Link className='nav-link' to="/logout">Logout</Link></li>
+        //             <li className="nav-item"><Link className='nav-link' to="/login">Login</Link></li>
+        //         </ul>
+        //     </div>
+        // </header>
+
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <Link className="navbar-brand ms-2 fs-2 fw-bold text-black" to="/welcome/Kaiqi">Todo APP</Link>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/Kaiqi">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
     )
 }
 
 function FooterComponent() {
     return (
-        <div className="footer">
-            <hr/> Header 
-        </div>
+        <footer className="footer">
+            <div className="container">
+            Your Footer
+            </div>
+        </footer>
     )
 }
 
